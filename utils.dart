3eb0f8b4e35445bb3530c698c28c5420c8f3e795
AnaCarol2707 <128806088+AnaCarol2707@users.noreply.github.com>
2024-09-28 13:58:32 -0300

    Delete utils.dart

--- utils.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Utils {
-  static const Color eurofarmaBlue = Color.fromRGBO(0, 72, 142, 1);
-  static const Color eurofarmaYellow = Color(0xFFFFF200);
-}
